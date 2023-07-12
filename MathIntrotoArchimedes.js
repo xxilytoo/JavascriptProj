526aaf02f9579dfeaf06d6d31ca3397963e47a95
xxilytoo <107663344+xxilytoo@users.noreply.github.com>
2023-07-11 20:16:48 -0500

    Create MathIntrotoArchimedes.js
    Math History Application

---
+++ MathIntrotoArchimedes.js
@@ -0,0 +1,75 @@
+onEvent("archimedesButton", "click", function( ) {
+  setScreen("archimedesScreen");
+  playSound("sound://category_achievements/lighthearted_bonus_objective_1.mp3", false);
+});
+onEvent("homeButton", "click", function( ) {
+  setScreen("mainMenu");
+  playSound("sound://category_achievements/lighthearted_bonus_objective_3.mp3", false);
+});
+onEvent("homeButton2", "click", function( ) {
+  setScreen("mainMenu");
+  playSound("sound://category_achievements/lighthearted_bonus_objective_3.mp3", false);
+});
+onEvent("homeButton3", "click", function( ) {
+  setScreen("mainMenu");
+  playSound("sound://category_achievements/lighthearted_bonus_objective_3.mp3", false);
+});
+onEvent("euclidButton", "click", function( ) {
+  setScreen("euclidScreen");
+  playSound("sound://category_achievements/lighthearted_bonus_objective_1.mp3", false);
+});
+onEvent("pythagorasButton", "click", function( ) {
+  setScreen("pythagorasScreen");
+  playSound("sound://category_achievements/lighthearted_bonus_objective_1.mp3", false);
+});
+onEvent("quizButton1", "click", function( ) {
+  setScreen("quizScreen");
+});
+onEvent("quizButton2", "click", function( ) {
+  setScreen("question1");
+});
+onEvent("pythagoras1", "click", function( ) {
+  setScreen("correct1");
+});
+onEvent("euclid1", "click", function( ) {
+  setScreen("wrong1");
+});
+onEvent("archimedes1", "click", function( ) {
+  setScreen("wrong1");
+});
+onEvent("backButton1", "click", function( ) {
+  setScreen("question1");
+});
+onEvent("nextQuestion1", "click", function( ) {
+  setScreen("question2");
+});
+onEvent("euclid2", "click", function( ) {
+  setScreen("wrong2");
+});
+onEvent("pythagoras2", "click", function( ) {
+  setScreen("wrong2");
+});
+onEvent("archimedes2", "click", function( ) {
+  setScreen("correct2");
+});
+onEvent("backButton2", "click", function( ) {
+  setScreen("question2");
+});
+onEvent("nextQuestion2", "click", function( ) {
+  setScreen("question3");
+});
+onEvent("euclid3", "click", function( ) {
+  setScreen("correct3");
+});
+onEvent("pythagoras3", "click", function( ) {
+  setScreen("wrong3");
+});
+onEvent("archimedes3", "click", function( ) {
+  setScreen("wrong3");
+});
+onEvent("backButton3", "click", function( ) {
+  setScreen("question3");
+});
+onEvent("mainMenu3", "click", function( ) {
+  setScreen("mainMenu");
+});
